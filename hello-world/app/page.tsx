b69199d9c264935183899c5a6d2d41af89f6252f
thaisivelazquez <thaisivelazquez@gmail.com>
2026-02-05 16:55:36 -0500

    should work now

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -1,3 +1,35 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabase/client'
+
 export default function Page() {
-  return <h1>Hello World!</h1>;
+    const [tables, setTables] = useState<any[]>([])
+
+    useEffect(() => {
+        supabase
+            .from('caption_examples')
+            .select('*')
+            .then(({ data }) => setTables(data ?? []))
+    }, [])
+
+    return (
+        <div>
+            <h1>caption_examples tables</h1>
+            <ul>
+                {tables.map((table: any) => (
+                    <li key={table.id}>
+                        <strong>ID:</strong> {table.id}<br />
+                        <strong>Date:</strong> {table.created_datetime_utc}<br />
+                        <strong>Date modified:</strong> {table.modified_created_datetime_utc}<br />
+                        <strong>Description:</strong> {table.image_description}<br />
+                        <strong>Caption:</strong> {table.caption}<br />
+                        <strong>Explanation:</strong> {table.explanation}<br />
+                        <strong>Priority:</strong> {table.priority}<br />
+                        <strong>image id:</strong> {table.image_id}<br />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
